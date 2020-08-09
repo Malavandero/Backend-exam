474590ecf85c7d9e61eecd6e17a28b2972599302
Rafael <rlg286@gmail.com>
2020-08-09 21:44:45 +0200

    install dependencies

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+composer require symfony/serializer
+composer require nelmio/api-doc-bundle:^3.0
+composer require --dev symfony/phpunit-bridge
+composer require symfony/orm-pack
+composer require --dev symfony/maker-bundle
+composer require annotations
+composer require --dev dama/doctrine-test-bundle
